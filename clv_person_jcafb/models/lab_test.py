cbcb81dfca9675eb61b4f37de57f065934132847
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-15 10:25:29 -0300

    [REF] clv_person_jcafb

--- clv_person_jcafb/models/lab_test.py
+++ clv_person_jcafb/models/lab_test.py
@@ -1,49 +1,11 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
 
 class Person(models.Model):
-    _inherit = 'clv.person'
-
-    document_ids = fields.One2many(
-        string='Documents',
-        comodel_name='clv.document',
-        compute='_compute_document_ids_and_count',
-    )
-    count_documents = fields.Integer(
-        compute='_compute_document_ids_and_count',
-    )
-
-    @api.multi
-    def _compute_document_ids_and_count(self):
-        for record in self:
-            documents = self.env['clv.document'].search([
-                ('ref_id', '=', self._name + ',' + str(record.id)),
-            ])
-            record.count_documents = len(documents)
-            record.document_ids = [(6, 0, documents.ids)]
-
-
-class Person_2(models.Model):
     _inherit = 'clv.person'
 
     lab_test_request_ids = fields.One2many(
@@ -64,10 +26,6 @@ class Person_2(models.Model):
             record.count_lab_test_requests = len(lab_test_requests)
             record.lab_test.request_ids = [(6, 0, lab_test_requests.ids)]
 
-
-class Person_3(models.Model):
-    _inherit = 'clv.person'
-
     lab_test_result_ids = fields.One2many(
         string='Lab Test Results',
         comodel_name='clv.lab_test.result',
@@ -85,10 +43,6 @@ class Person_3(models.Model):
             ])
             record.count_lab_test_results = len(lab_test_results)
             record.lab_test.result_ids = [(6, 0, lab_test_results.ids)]
-
-
-class Person_4(models.Model):
-    _inherit = 'clv.person'
 
     lab_test_report_ids = fields.One2many(
         string='Lab Test Reports',
