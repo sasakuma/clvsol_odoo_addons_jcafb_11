e8de2a45d3244ff1b6cf3f2394849d8335338aaf
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-14 21:38:12 -0300

    [IMP] clv_person_jcafb

--- clv_person_jcafb/models/person.py
+++ clv_person_jcafb/models/person.py
@@ -41,3 +41,69 @@ class Person(models.Model):
             ])
             record.count_documents = len(documents)
             record.document_ids = [(6, 0, documents.ids)]
+
+
+class Person_2(models.Model):
+    _inherit = 'clv.person'
+
+    lab_test_request_ids = fields.One2many(
+        string='Lab Test Requests',
+        comodel_name='clv.lab_test.request',
+        compute='_compute_lab_test_request_ids_and_count',
+    )
+    count_lab_test_requests = fields.Integer(
+        compute='_compute_lab_test_request_ids_and_count',
+    )
+
+    @api.multi
+    def _compute_lab_test_request_ids_and_count(self):
+        for record in self:
+            lab_test_requests = self.env['clv.lab_test.request'].search([
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ])
+            record.count_lab_test_requests = len(lab_test_requests)
+            record.lab_test.request_ids = [(6, 0, lab_test_requests.ids)]
+
+
+class Person_3(models.Model):
+    _inherit = 'clv.person'
+
+    lab_test_result_ids = fields.One2many(
+        string='Lab Test Results',
+        comodel_name='clv.lab_test.result',
+        compute='_compute_lab_test_result_ids_and_count',
+    )
+    count_lab_test_results = fields.Integer(
+        compute='_compute_lab_test_result_ids_and_count',
+    )
+
+    @api.multi
+    def _compute_lab_test_result_ids_and_count(self):
+        for record in self:
+            lab_test_results = self.env['clv.lab_test.result'].search([
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ])
+            record.count_lab_test_results = len(lab_test_results)
+            record.lab_test.result_ids = [(6, 0, lab_test_results.ids)]
+
+
+class Person_4(models.Model):
+    _inherit = 'clv.person'
+
+    lab_test_report_ids = fields.One2many(
+        string='Lab Test Reports',
+        comodel_name='clv.lab_test.report',
+        compute='_compute_lab_test_report_ids_and_count',
+    )
+    count_lab_test_reports = fields.Integer(
+        compute='_compute_lab_test_report_ids_and_count',
+    )
+
+    @api.multi
+    def _compute_lab_test_report_ids_and_count(self):
+        for record in self:
+            lab_test_reports = self.env['clv.lab_test.report'].search([
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ])
+            record.count_lab_test_reports = len(lab_test_reports)
+            record.lab_test.report_ids = [(6, 0, lab_test_reports.ids)]
