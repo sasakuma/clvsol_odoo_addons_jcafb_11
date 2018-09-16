c0756d6e12910e3073c1cc46632a4de2703b575a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-16 17:27:44 -0300

    [IMP] clv_address_jcafb (Substitue "event_attendee" for "event")

---
+++ clv_address_jcafb/models/event_attendee.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class Address(models.Model):
+    _inherit = 'clv.address'
+
+    event_attendee_ids = fields.One2many(
+        string='Event Attendees',
+        comodel_name='clv.event.attendee',
+        compute='_compute_event_attendee_ids_and_count',
+    )
+    count_events = fields.Integer(
+        compute='_compute_event_attendee_ids_and_count',
+    )
+
+    @api.multi
+    def _compute_event_attendee_ids_and_count(self):
+        for record in self:
+            event_attendees = self.env['clv.event.attendee'].search([
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ])
+            record.count_events = len(event_attendees)
+            record.event_attendee_ids = [(6, 0, event_attendees.ids)]
