8b24a4d7ccffa5698c386a0307bbd476953e153a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-20 20:51:00 -0300

    [IMP] clv_base_jcafb (ADD views/export_menu_view.xml)

--- clv_base_jcafb/__manifest__.py
+++ clv_base_jcafb/__manifest__.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright (C) 2017-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Module (customizations for CLVhealth-JCAFB Solution)',
     'summary': 'Base Module Module customizations for CLVhealth-JCAFB Solution.',
-    'version': '3.0.0',
+    'version': '4.0.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
@@ -36,6 +20,7 @@
         'views/health_menu_view.xml',
         # 'views/report_menu_view.xml',
         # 'views/data_export_menu_view.xml',
+        'views/export_menu_view.xml',
         # 'data/annotation_seq.xml',
     ],
     'demo': [],
