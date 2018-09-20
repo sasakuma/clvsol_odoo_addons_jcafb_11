fb0631bb9a1fd33f0b56ebbd53bebf38539d4c7a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-09-20 13:24:53 -0300

    [ADD] clv_file_system_jcafb

---
+++ clv_file_system_jcafb/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'File System (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'File System Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_file_system',
+    ],
+    'data': [
+        'views/file_system_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
