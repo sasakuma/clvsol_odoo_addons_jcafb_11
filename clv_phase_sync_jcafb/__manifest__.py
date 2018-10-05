68450b5c586abac6bb79e6dd59bc5059807566fa
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-10-05 14:48:36 -0300

    [ADD] clv_phase_sync_jcafb

---
+++ clv_phase_sync_jcafb/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Phase External Sync (for CLVhealth-JCAFB Solution)',
+    'summary': 'Phase External Sync Module used in CLVhealth-JCAFB Solution.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_phase',
+        'clv_external_sync',
+    ],
+    'data': [
+        'views/phase_view.xml',
+        'data/external_sync_template.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
